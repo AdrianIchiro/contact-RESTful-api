3be5aaecd35e49c8784a3ca95ffe45bf9ff10989
AdrianIchiro <adrianfahren10@gmail.com>
2024-03-16 09:40:49 +0700

    manipulate schema

--- prisma/migrations/20240315170911_create_table_users/migration.sql
+++ prisma/migrations/20240315170911_create_table_users/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
 CREATE TABLE `users` (
     `username` VARCHAR(100) NOT NULL,
-    `email` VARCHAR(100) NOT NULL,
     `password` VARCHAR(100) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
     `token` VARCHAR(100) NULL,
